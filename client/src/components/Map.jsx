2c3266233412f78b6d417615ef41577c0118b85c
jainsimran <itsimmi5@gmail.com>
2020-02-20 23:51:40 -0500

    working info window in map section.

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,6 @@
 import GoogleMapReact from 'google-map-react';
 import React, { Component } from 'react';
+import MapMarker from './MapMarker';
 
 export default class Map extends Component {
   constructor() {
@@ -9,54 +10,35 @@ export default class Map extends Component {
         lat: 43.6708,
         lng: -79.3899
       },
-      zoom: 10,
-      showInfo: false
+      zoom: 3
     }
-  }
-
-   showInfo = () => {
-    this.setState({ showInfo: !this.state.showInfo });
   }
 
   render() {
-    const Marker = () => {
-      return (
-        <section className='labelMaker' onClick={this.showInfo}> 
-          {this.state.showInfo ? <div className='infoWindow'><h1>test</h1></div> : null}
-        </section>
-      );
-    }
-
-
-    const Poimarker = () => {
-      return (
-        <section style={{ height: '85vh', width: '100%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: `AIzaSyCk79ODgAFqY9di7oVX5Zh_zdIyecxZvks` }}
-            defaultCenter={this.state.center}
-            defaultZoom={this.state.zoom}
-          >
-            {
-              this.props.data.map((item, idx) => {
-                return (
-                  <Marker
-                    key={idx}
-                    lat={item.lat}
-                    lng={item.lng}
-                    title={item.name}
-                  >
-                  </Marker>
-                );
-              })
-            }
-          </GoogleMapReact>
-        </section>
-      );
-    }
     return (
-      <section>
-        <Poimarker />
+      <section style={{ height: '85vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: `AIzaSyCk79ODgAFqY9di7oVX5Zh_zdIyecxZvks` }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
+        >
+          {
+            this.props.data.map((item, idx) => {
+              return (
+                <MapMarker
+                  key={idx}
+                  lat={item.lat}
+                  lng={item.lng}
+                  poi={item}
+
+                >
+                </MapMarker>
+              );
+            })
+          }
+        </GoogleMapReact>
       </section>
     );
+
   }
 }
